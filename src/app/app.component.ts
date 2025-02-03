d91a6843ebcdf0919bc3691219633ebfff6ee435
masud467 <masudpfrnt@gmail.com>
2025-02-03 10:01:09 +0600

    setup basic file structure and basic concept of angular.

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { IUser } from './user.modal';
+import { NgClass, NgFor, NgStyle } from '@angular/common';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, NgFor,NgStyle,NgClass],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'my-first-angular';
+
+  users:IUser[]=[
+    {
+      name:'John',
+      age:20,
+      image:'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      email:'john@gmail.com',
+      isActive:true
+    },
+    {
+      name:'Jane',
+      age:21,
+      image:'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      email:'jane@gmail.com',
+      isActive:false
+    },
+    {
+      name:'Jack',
+      age:22,
+      image:'https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      email:'jack@gmail.com',
+      isActive:true
+    }
+  ]
+  
+}
---
+++ src/app/user.modal.ts
@@ -0,0 +1,7 @@
+export interface IUser{
+    name:string;
+    age:number;
+    image:string;
+    email:string;
+    isActive:boolean;
+}
