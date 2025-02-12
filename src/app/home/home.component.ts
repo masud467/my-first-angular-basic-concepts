97f05212ae7db692a11a2709e5f4eb269c682c9e
masud467 <masudpfrnt@gmail.com>
2025-02-12 12:19:54 +0600

    added card and button.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule,MatDividerModule,MatTooltipModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
